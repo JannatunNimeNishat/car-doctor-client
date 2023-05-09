34390424d97cd0ae9eff4f46c11421135c5e72b7
JannatunNimeNishat <nishatnime100@gmail.com>
2023-05-10 00:58:45 +0600

    Basic routes layout and navbar with banner added

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+import router from './Routes/Routes'
+import { RouterProvider } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // fixed the width for every page
+  <div className='max-w-7xl mx-auto'>
+    <React.StrictMode>
+      <RouterProvider router={router}></RouterProvider>
+    </React.StrictMode>,
+  </div>
+)
