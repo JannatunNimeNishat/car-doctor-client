1c06c83c1cb22a9d775115c9edb3cf16ebb3a2ce
JannatunNimeNishat <nishatnime100@gmail.com>
2023-05-10 19:06:47 +0600

    login resgister and onAuthSateChaged added

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import img from '../../assets/images/login/login.svg'
+import { AuthContext } from '../../providers/AuthProvider';
 const SignUp = () => {
-
+    const {createUser} = useContext(AuthContext);
     const handleSignUp = event =>{
         event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name,email,password);
+        //
+        createUser(email,password)
+        .then(result =>{
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(error =>{
+            console.log(error.message);
+        })
     }
 
 
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,16 +1,42 @@
-import React, { createContext } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 export const AuthContext = createContext();
-import { getAuth } from 'firebase/auth'
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth'
 import app from '../firebase/firebase.config';
 
 const AuthProvider = ({ children }) => {
 
     const auth = getAuth(app)
+    const [user,setUser] = useState();
+    const [loading,setLoading]= useState(true);
+
+    //Register
+    const createUser = (email,password)=>{
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth,email,password);
+    }
+    //login
+    const singIn = (email,password)=>{
+        setLoading(true);
+        return signInWithEmailAndPassword(email,password)
+    }
+    useEffect(()=>{
+        const unSubscribe = onAuthStateChanged(auth, currentUser =>{
+            setUser(currentUser)
+            console.log('current user',currentUser);
+            setLoading(false)
+        })
+        return ()=>{
+            unSubscribe();
+        }
+    },[])
 
     const authInfo = {
-
+        user,
+        loading,
+        createUser,
+        singIn
     }
-    
+
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
